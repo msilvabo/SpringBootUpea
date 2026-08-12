ed2dee2dbd928ce69863f820e67ad975dbd4dca6
msilvabo <silva.moises@gmail.com>
2026-08-12 12:13:01 -0400

    Pageable with Filtered Table products

--- src/main/java/com/postgrado/ecommerce/EcommerceApplication.java
+++ src/main/java/com/postgrado/ecommerce/EcommerceApplication.java
@@ -1,7 +1,5 @@
 package com.postgrado.ecommerce;
 
-import com.postgrado.ecommerce.repository.CategoryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/com/postgrado/ecommerce/repository/RoleRepository.java
+++ src/main/java/com/postgrado/ecommerce/repository/RoleRepository.java
@@ -2,7 +2,6 @@ package com.postgrado.ecommerce.repository;
 
 import com.postgrado.ecommerce.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
