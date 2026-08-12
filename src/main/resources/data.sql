2f38b410c33a8de9e7b25ff60ab19a31720684b3
msilvabo <silva.moises@gmail.com>
2026-08-12 10:14:42 -0400

    initial data from sql archive

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+-- Defaul categories --
+
+INSERT INTO categories (id, description, name) VALUES ('9f70510a-06c3-4676-b492-403dd6a41281', 'Mochila Viajes', 'Travel');
+INSERT INTO categories (id, description, name) VALUES ('b57b9f94-f38e-4e1d-b56d-69a7fce3b5ce', 'Mochila deportivas', 'Sport');
+INSERT INTO categories (id, description, name) VALUES ('48e960ff-a6f6-4c27-92de-840b14b7b5f1', 'Mochila de Trabajo', 'Work');
